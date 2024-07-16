cafc0cb4925aacda9d734be5bebbf94e2643f061
msck72 <chaitanya.mangipudi2002@gmail.com>
2024-07-16 15:59:06 -0600

    added resnet model for cifar

--- models.py
+++ models.py
@@ -12,7 +12,8 @@ def create_model(cfg, model_rate, device= torch.device('cpu'))  :
     if(cfg.dataset == 'mnist'):
         return conv(model_rate, [1, 28, 28], 10, cfg.hidden_layers, device)
     elif(cfg.dataset == 'cifar10'):
-        return conv(model_rate, [3, 32, 32], 10, cfg.hidden_layers, device)
+        # return conv(model_rate, [3, 32, 32], 10, cfg.hidden_layers, device)
+        return ResNet(cfg.hidden_layers, model_rate, device)
     else:
         raise ValueError("Sorry no dataset_name is known")
     
@@ -111,6 +112,67 @@ def conv(
     model = Conv(hidden_size=hidden_size, data_shape=data_shape, classes_size=classes_size)
     return model.to(device)
 
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
+
+    def forward(self, x):
+        out = torch.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = torch.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, hidden_layers , num_blocks, num_classes=10, model_rate=1):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, hidden_layers[0] * model_rate, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, hidden_layers[1] * model_rate, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, hidden_layers[2] * model_rate, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, hidden_layers[3] * model_rate, num_blocks[3], stride=2)
+        self.linear = nn.Linear(hidden_layers[3] * model_rate * block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = torch.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = torch.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+def ResNet18(hidden_layers, model_rate = 1, device = "cpu"):
+    return ResNet(hidden_layers, BasicBlock, [2, 2, 2, 2], model_rate)
 
 
 def get_state_dict_from_param(model, parameters):
